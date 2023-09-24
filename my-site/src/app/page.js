3bc8771484c95fdfdb40aff37a87c6ab2fd030e2
alanescritorio <alangonzalez9407@gmail.com>
2023-09-23 22:11:22 -0600

    Examen version 4

--- my-site/src/app/page.js
+++ my-site/src/app/page.js
@@ -1,14 +1,16 @@
 import Image from 'next/image'
-import styles from './page.module.css'
+import styles from "./page.module.css"
+import Navbar from "./components/navbar";
 
 export default function Home() {
   return (
     <main>
-      <header className={styles.header}>
+      <Navbar></Navbar>
+      <header id="header" className={styles.header}>
         <div>
           <h1>
             <span>
-              Hola<span className={styles.yellowText}>.</span>
+              Hola<span className={styles.yellowText}> B)</span>
             </span>
             <br />
             <span>
@@ -19,29 +21,29 @@ export default function Home() {
           </h1>
         </div>
       </header>
-      <section className={styles.infoSection}>
+      <section id="info" className={styles.infoSection}>
         <img
           src = "/spider-man-36.png"
           alt = "Imagen"
           className = {styles.image}
         />
         <div className={styles.infoContainer}>
-          <span className={styles.title + "" + styles.yellowText}>Alan</span>
+          <span className={styles.title + "" + styles.greenText}>Alan</span>
           <br />
           <span className={styles.title}>Gonzalez</span>
           <div className={styles.list}>
             <ul>
               <li>
-                <span className={styles.grayText}>Age: </span> 27
+                <span className={styles.grayText}>Edad: </span> 16
               </li>
               <li>
-                <span className={styles.grayText}>Nationality: </span>German
+                <span className={styles.grayText}>Nacionalidad: </span>Persa
               </li>
               <li>
-                <span className={styles.grayText}>Skill set: </span> Project Management and Financial Performance
+                <span className={styles.grayText}>Habilidades: </span> Muchas
               </li>
               <li>
-                <span className={styles.grayText}>Languages: </span> English, German
+                <span className={styles.grayText}>Idiomas: </span> Español, Ingles, Kaqchikel, Peruano
               </li>
             </ul>
           </div>
